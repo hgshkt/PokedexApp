a87fc8eb00f31244f0aa17db4b990d8923459372
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-02 20:30:55 +0300

    add list view model

---
+++ app/src/main/java/com/hgshkt/pokedex/ui/list/ListUseCases.kt
@@ -0,0 +1,7 @@
+package com.hgshkt.pokedex.ui.list
+
+import com.hgshkt.domain.useCases.LoadPokemonsUseCase
+
+data class ListUseCases(
+    val loadPokemonsUseCase: LoadPokemonsUseCase
+)
---
+++ app/src/main/java/com/hgshkt/pokedex/ui/list/ListViewModel.kt
@@ -0,0 +1,22 @@
+package com.hgshkt.pokedex.ui.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.hgshkt.domain.model.Pokemon
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+class ListViewModel(
+    private val listUseCases: ListUseCases
+) : ViewModel() {
+    val pokemons: Flow<PagingData<Pokemon>> = Pager(
+        PagingConfig(pageSize = 20)
+    ) {
+        listUseCases.loadPokemonsUseCase.execute()
+    }.flow.stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
+}
