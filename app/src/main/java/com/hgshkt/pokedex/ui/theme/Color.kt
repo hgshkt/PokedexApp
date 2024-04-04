a59b4446cc33d12e2177cb4e89c2f19df6cf5239
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-04 22:44:51 +0300

    change pokemon card background color

--- app/src/main/java/com/hgshkt/pokedex/ui/theme/Color.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/theme/Color.kt
@@ -10,4 +10,4 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val pokemonCardBackgroundColor = Color(0xFFD5F9FF)
+val pokemonCardBackgroundColor = Color(0xFFEBF5FC)
