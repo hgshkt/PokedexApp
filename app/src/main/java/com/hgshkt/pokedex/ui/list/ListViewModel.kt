0901ffb3b7ea7e9ba03473a37ef2bf9916cdbbaf
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-04 16:24:47 +0300

    commented unused part of FinalPokemonDTO

--- app/src/main/java/com/hgshkt/pokedex/ui/list/ListViewModel.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/list/ListViewModel.kt
@@ -2,23 +2,15 @@ package com.hgshkt.pokedex.ui.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import com.hgshkt.domain.model.Pokemon
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(
-    private val listUseCases: ListUseCases
+    private val useCases: ListUseCases
 ) : ViewModel() {
-    val pokemons: Flow<PagingData<Pokemon>> = Pager(
-        PagingConfig(pageSize = 20)
-    ) {
-        listUseCases.loadPokemonsUseCase.execute()
-    }.flow.stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
+
+    val pokemons = useCases.loadPokemonsUseCase.execute()
+        .cachedIn(viewModelScope)
 }
--- domain/src/main/java/com/hgshkt/domain/useCases/LoadPokemonsUseCase.kt
+++ domain/src/main/java/com/hgshkt/domain/useCases/LoadPokemonsUseCase.kt
@@ -1,14 +1,21 @@
 package com.hgshkt.domain.useCases
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import com.hgshkt.domain.data.PokemonRemoteRepository
 import com.hgshkt.domain.model.Pokemon
+import kotlinx.coroutines.flow.Flow
 
 
 class LoadPokemonsUseCase(
     private val remoteRepository: PokemonRemoteRepository
 ) {
-    fun execute(): PagingSource<Int, Pokemon> {
-        return remoteRepository.loadPokemons()
+    fun execute(): Flow<PagingData<Pokemon>> = Pager(
+        PagingConfig(pageSize = 20, prefetchDistance = 20)
+    ) {
+        remoteRepository.loadPokemons()
     }
+        .flow
 }
