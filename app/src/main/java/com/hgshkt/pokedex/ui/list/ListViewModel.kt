3d6f0d1999cf64096e401cab820afbb85f8e32c8
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-14 04:29:19 +0300

    change view model

--- app/src/main/java/com/hgshkt/pokedex/ui/list/ListViewModel.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/list/ListViewModel.kt
@@ -2,8 +2,12 @@ package com.hgshkt.pokedex.ui.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.hgshkt.domain.model.Pokemon
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +15,16 @@ class ListViewModel @Inject constructor(
     private val useCases: ListUseCases
 ) : ViewModel() {
 
-    val pokemons = useCases.loadPokemonsUseCase.execute()
-        .cachedIn(viewModelScope)
+    private val _pokemonsState: MutableStateFlow<PagingData<Pokemon>> = MutableStateFlow(value = PagingData.empty())
+    val pokemonsState: MutableStateFlow<PagingData<Pokemon>> get() = _pokemonsState
+
+    init {
+        viewModelScope.launch {
+            useCases.loadPokemonsUseCase.execute()
+                .cachedIn(viewModelScope)
+                .collect {
+                    _pokemonsState.value = it
+                }
+        }
+    }
 }
