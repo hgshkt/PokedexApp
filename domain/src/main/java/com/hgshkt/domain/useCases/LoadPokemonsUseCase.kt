0ee410da2392d8e92d22883925a84dd1237386c1
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-14 04:23:03 +0300

    change LoadPokemonsUseCase to fetching Flow<PagingData<Pokemon>>

--- domain/src/main/java/com/hgshkt/domain/useCases/LoadPokemonsUseCase.kt
+++ domain/src/main/java/com/hgshkt/domain/useCases/LoadPokemonsUseCase.kt
@@ -1,39 +1,14 @@
 package com.hgshkt.domain.useCases
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.map
-import com.hgshkt.domain.data.AbilityRemoteRepository
-import com.hgshkt.domain.data.PokemonRemoteRepository
-import com.hgshkt.domain.data.mapper.toAbility
-import com.hgshkt.domain.data.mapper.toPokemon
+import com.hgshkt.domain.data.PokemonRepository
 import com.hgshkt.domain.model.Pokemon
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 
 class LoadPokemonsUseCase(
-    private val pokemonRemoteRepository: PokemonRemoteRepository,
-    private val abilityRemoteRepository: AbilityRemoteRepository
+    private val pokemonRepository: PokemonRepository
 ) {
-    fun execute(): Flow<PagingData<Pokemon>> = Pager(
-        PagingConfig(pageSize = 20, prefetchDistance = 20)
-    ) {
-        pokemonRemoteRepository.loadPokemons()
-    }
-        .flow
-        .map { pagingData ->
-            pagingData.map { dpokemon ->
-                with(dpokemon) {
-
-                    val abilities = abilitiesUrl.map { url ->
-                        abilityRemoteRepository.getAbility(url).toAbility()
-                    }
-                    toPokemon(abilities)
-
-                }
-            }
-        }
+    suspend fun execute(): Flow<PagingData<Pokemon>> = pokemonRepository.getPokemons()
 
 }
