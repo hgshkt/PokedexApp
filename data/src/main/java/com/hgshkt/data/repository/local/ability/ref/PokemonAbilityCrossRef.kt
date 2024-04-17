acb286bcf7d56fab00258d2347b9bce44a0874ff
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-17 13:12:03 +0300

    update PokemonAbilityCrossRef entity

--- data/src/main/java/com/hgshkt/data/repository/local/ability/ref/PokemonAbilityCrossRef.kt
+++ data/src/main/java/com/hgshkt/data/repository/local/ability/ref/PokemonAbilityCrossRef.kt
@@ -8,20 +8,7 @@ import com.hgshkt.data.repository.local.pokemon.PokemonEntity
 
 @Entity(
     tableName = "pokemonabilityrefs",
-    primaryKeys = ["pokemonId", "abilityId"],
-    foreignKeys = [
-        ForeignKey(
-            entity = PokemonEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["pokemonId"]
-        ),
-        ForeignKey(
-            entity = AbilityEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["abilityId"]
-        )
-    ],
-    indices = [Index("pokemonId"), Index("abilityId")]
+    primaryKeys = ["pokemonId", "abilityId"]
 )
 data class PokemonAbilityCrossRef(
     var pokemonId: Int,
