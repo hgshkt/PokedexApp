88d8484643488370c17e1d99055b3821dbc2bb47
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-19 14:21:38 +0300

    update detail screen

--- app/src/main/java/com/hgshkt/pokedex/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/detail/DetailScreen.kt
@@ -1,73 +1,154 @@
 package com.hgshkt.pokedex.ui.detail
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.hgshkt.domain.model.Ability
 import com.hgshkt.domain.model.Pokemon
+import com.hgshkt.domain.model.Stats
+import com.hgshkt.domain.model.Type
 import com.hgshkt.pokedex.ui.custom.text.AutoResizedText
-import com.hgshkt.pokedex.ui.theme.pokemonNameStyle
+
+private val idStyle = TextStyle(fontSize = 20.sp)
+private val pokemonNameStyle = TextStyle(fontSize = 40.sp, fontWeight = FontWeight.Bold)
+
+private val abilityBackground = Color(0xFFD3F0FF)
+private val statsBorder = Color(0xFFD3F0FF)
+
+@Preview
+@Composable
+fun DetailScreenPreview() {
+    val pokemon = Pokemon(
+        id = 1,
+        name = "Name",
+        imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        abilities = listOf(
+            Ability(id = 1, name = "First Ability", effect = "First ability do something"),
+            Ability(id = 1, name = "Second Ability", effect = "Second ability do something"),
+        ),
+        types = listOf(Type.POISON, Type.DARK),
+        stats = Stats(
+            hp = 13,
+            attack = 45,
+            defense = 100,
+            specialAttack = 99,
+            specialDefense = 75,
+            speed = 26
+        ),
+        weight = 10,
+        height = 90
+    )
+    DetailScreen(pokemon)
+}
 
 @Composable
 fun DetailScreen(pokemon: Pokemon) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        AsyncImage(
-            model = pokemon.imageUrl,
-            contentDescription = "pokemon image",
-            modifier = Modifier
-                .widthIn(0.dp, 400.dp)
-                .fillMaxWidth()
-        )
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            PokemonTitle(
+    with(pokemon) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            AsyncImage(
+                model = imageUrl,
+                contentDescription = "pokemon image",
                 modifier = Modifier
-                    .padding(4.dp),
-                id = pokemon.id,
-                name = pokemon.name
+                    .widthIn(0.dp, 400.dp)
+                    .fillMaxWidth()
             )
-            AbilityList(
-                modifier = Modifier.padding(8.dp),
-                abilities = pokemon.abilities
-            )
-            Text("Types:")
-            Row {
-                pokemon.types.forEach { type ->
-                    PokemonType(type.toUIType())
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "№$id", style = idStyle)
+                    AutoResizedText(text = name, style = pokemonNameStyle)
                 }
-            }
-            Column(modifier = Modifier.padding(4.dp)) {
-                Text("Stats:")
-                Row {
-                    Column {
-                        Text("HP: ${pokemon.stats.hp}")
-                        Text("Attack: ${pokemon.stats.attack}")
-                        Text("Defense: ${pokemon.stats.defense}")
-                    }
-                    Spacer(modifier = Modifier.width(20.dp))
-                    Column {
-                        Text("Sp. Atk: ${pokemon.stats.specialAttack}")
-                        Text("Sp. Def: ${pokemon.stats.specialDefense}")
-                        Text("Speed: ${pokemon.stats.speed}")
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    types.forEach { type ->
+                        PokemonType(type.toUIType())
                     }
                 }
-            }
-            Row {
-                Text("Height: ${pokemon.height}")
-                Text("Weight: ${pokemon.weight}")
+                Spacer(modifier = Modifier.height(12.dp))
+                AbilityList(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    abilities = abilities
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .border(4.dp, statsBorder, shape = RoundedCornerShape(16.dp))
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    val statSize = 26.sp
+                    with(stats) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+
+                            Column {
+                                Text("HP: $hp", fontSize = statSize)
+                                Text("Attack: $attack", fontSize = statSize)
+                                Text("Defense: $defense", fontSize = statSize)
+                            }
+                            Spacer(modifier = Modifier.width(20.dp))
+                            Column {
+                                Text("Sp. Atk: $specialAttack", fontSize = statSize)
+                                Text("Sp. Def: $specialDefense", fontSize = statSize)
+                                Text("Speed: $speed", fontSize = statSize)
+                            }
+
+                        }
+                        Spacer(modifier = Modifier.height(12.dp))
+                        Text("Total: $total", fontSize = statSize)
+                    }
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    val fontSize = 24.sp
+                    Text("Height: $height", fontSize = fontSize)
+                    Text("Weight: $weight", fontSize = fontSize)
+                }
             }
         }
     }
@@ -75,30 +156,40 @@ fun DetailScreen(pokemon: Pokemon) {
 
 @Composable
 fun PokemonType(type: UIType) {
-    Box(modifier = Modifier.padding(4.dp).background(type.color)) {
-        Text(type.text)
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(type.backgroundColor)
+            .padding(horizontal = 22.dp, vertical = 12.dp)
+    ) {
+        Text(
+            text = type.text,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = type.textColor
+        )
     }
 }
 
 @Composable
 fun AbilityList(modifier: Modifier, abilities: List<Ability>) {
-    Column {
-        Text("Abilities:")
-        abilities.forEach { ability ->
-            Text(ability.name)
-            Text(ability.effect)
-        }
-    }
-}
-
-@Composable
-fun PokemonTitle(modifier: Modifier = Modifier, id: Int, name: String) {
-    Box(
-        modifier = modifier
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column {
-            Text(text = "#$id")
-            AutoResizedText(text = name, style = pokemonNameStyle)
+        Text(text = "Abilities:", fontSize = 26.sp)
+        abilities.forEach { ability ->
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(abilityBackground)
+                    .padding(8.dp),
+            ) {
+                Text(ability.name, fontSize = 26.sp, fontWeight = FontWeight.Bold)
+                Text(ability.effect, fontSize = 22.sp)
+            }
         }
     }
 }
--- app/src/main/java/com/hgshkt/pokedex/ui/detail/UIType.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/detail/UIType.kt
@@ -4,25 +4,25 @@ import androidx.compose.ui.graphics.Color
 
 import com.hgshkt.domain.model.Type
 
-enum class UIType(val text: String, val color: Color) {
-    NORMAL("NORMAL", Color(0xFFC4A870)),
-    FIRE("FIRE", Color(0xFFFF8418)),
-    WATER("WATER", Color(0xFF6A8BEE)),
-    ELECTRIC("ELECTRIC", Color(0xFFFFE600)),
-    GRASS("GRASS", Color(0xFF86EF73)),
-    ICE("ICE", Color(0xFFA6F2F5)),
-    FIGHTING("FIGHTING", Color(0xFFFF5757)),
-    POISON("POISON", Color(0xFFA36CF8)),
-    GROUND("GROUND", Color(0xFFA8946B)),
-    FLYING("FLYING", Color(0xFF41E0FC)),
-    PHYSIC("PHYSIC", Color(0xFFEC3CD4)),
-    BUG("BUG", Color(0xFFBBE250)),
-    ROCK("ROCK", Color(0xFFC5B6A0)),
-    GHOST("GHOST", Color(0xFF8974AF)),
-    DRAGON("DRAGON", Color(0xFF8E73EF)),
-    DARK("DARK", Color(0xFF2A3155)),
-    STEEL("STEEL", Color(0xFF797979)),
-    FAIRY("FAIRY", Color(0xFFF7A5F2))
+enum class UIType(val text: String, val backgroundColor: Color, val textColor: Color) {
+    NORMAL("NORMAL", Color(0xFFC4A870), Color(0xFFFFFFFF)),
+    FIRE("FIRE", Color(0xFFFF8418), Color(0xFFFFFFFF)),
+    WATER("WATER", Color(0xFF6A8BEE), Color(0xFFFFFFFF)),
+    ELECTRIC("ELECTRIC", Color(0xFFFFE600), Color(0xFFC4A870)),
+    GRASS("GRASS", Color(0xFF86EF73), Color(0xFFFFFFFF)),
+    ICE("ICE", Color(0xFFA6F2F5), Color(0xFFFFFFFF)),
+    FIGHTING("FIGHTING", Color(0xFFFF5757), Color(0xFFFFFFFF)),
+    POISON("POISON", Color(0xFFA36CF8), Color(0xFFFFFFFF)),
+    GROUND("GROUND", Color(0xFFA8946B), Color(0xFFFFFFFF)),
+    FLYING("FLYING", Color(0xFF41E0FC), Color(0xFFFFFFFF)),
+    PHYSIC("PHYSIC", Color(0xFFEC3CD4), Color(0xFFFFFFFF)),
+    BUG("BUG", Color(0xFFBBE250), Color(0xFFFFFFFF)),
+    ROCK("ROCK", Color(0xFFC5B6A0), Color(0xFFFFFFFF)),
+    GHOST("GHOST", Color(0xFF8974AF), Color(0xFFFFFFFF)),
+    DRAGON("DRAGON", Color(0xFF8E73EF), Color(0xFFFFFFFF)),
+    DARK("DARK", Color(0xFF2A3155), Color(0xFFFFFFFF)),
+    STEEL("STEEL", Color(0xFF797979), Color(0xFFFFFFFF)),
+    FAIRY("FAIRY", Color(0xFFF7A5F2), Color(0xFFFFFFFF))
 }
 
 fun Type.toUIType(): UIType {
