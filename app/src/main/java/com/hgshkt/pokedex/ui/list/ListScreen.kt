6d49b1adaed89e50daefd1297a440d0b1c3b1c02
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-20 17:51:36 +0300

    circular indicator in pokemon list

--- app/src/main/java/com/hgshkt/pokedex/ui/list/ListScreen.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/list/ListScreen.kt
@@ -1,13 +1,18 @@
 package com.hgshkt.pokedex.ui.list
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
@@ -22,21 +27,61 @@ fun ListScreen(
 ) {
     val pokemons: LazyPagingItems<Pokemon> =
         viewModel.pokemonsState.collectAsLazyPagingItems()
-
-    LazyVerticalGrid(
-        modifier = Modifier.wrapContentWidth(),
-        columns = GridCells.Fixed(3),
-        horizontalArrangement = Arrangement.SpaceAround,
+    
+    if (
+        pokemons.loadState.append == LoadState.Loading
+        || pokemons.loadState.refresh == LoadState.Loading
+        || pokemons.loadState.prepend == LoadState.Loading
     ) {
-        items(
-            count = pokemons.itemCount,
-            key = pokemons.itemKey { it.id },
-            contentType = pokemons.itemContentType { "pokemons" }
-        ) { index ->
-            val pokemon = pokemons[index]
-            PokemonCard(
-                pokemon = pokemon,
-            ) { onItemClick(PokemonSaver(pokemon!!)) }
+        LoadingBox()
+    } 
+    
+    else if (
+        pokemons.loadState.append == LoadState.Loading
+        || pokemons.loadState.refresh == LoadState.Loading
+        || pokemons.loadState.prepend == LoadState.Loading
+    ) {
+        ErrorBox()
+    } 
+    
+    else {
+        LazyVerticalGrid(
+            modifier = Modifier.wrapContentWidth(),
+            columns = GridCells.Fixed(3),
+            horizontalArrangement = Arrangement.SpaceAround,
+        ) {
+            items(
+                count = pokemons.itemCount,
+                key = pokemons.itemKey { it.id },
+                contentType = pokemons.itemContentType { "pokemons" }
+            ) { index ->
+                val pokemon = pokemons[index]
+                PokemonCard(
+                    pokemon = pokemon,
+                ) { onItemClick(PokemonSaver(pokemon!!)) }
+            }
         }
+    }
+}
+
+@Composable
+fun ErrorBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Something went wrong...")
+    }
+}
+
+@Composable
+private fun LoadingBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
     }
 }
