a263ee7c01a582d33ebda436a646b489aa5886d2
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-04-23 11:38:15 +0300

    updated gradle dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.hgshkt.pokedex"
         minSdk = 30
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -56,21 +56,21 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":data"))
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     // Jetpack Compose
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.compose.material3:material3-window-size-class-android:1.2.1")
-    implementation("androidx.compose.material3.adaptive:adaptive:1.0.0-alpha10")
-    implementation("androidx.compose.material3.adaptive:adaptive-layout:1.0.0-alpha10")
-    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.0.0-alpha10")
+    implementation("androidx.compose.material3.adaptive:adaptive:1.0.0-alpha11")
+    implementation("androidx.compose.material3.adaptive:adaptive-layout:1.0.0-alpha11")
+    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.0.0-alpha11")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -88,12 +88,12 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.46.1")
     kapt("com.google.dagger:hilt-compiler:2.46.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     // Room
     val room_version = "2.6.1"
