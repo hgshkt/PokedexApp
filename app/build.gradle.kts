7f77f60cdcaa63e114336cf8960db19251ec8690
hgshkt <nichtojestvozxc@gmail.com>
2024-08-13 18:27:18 +0300

    version code changed to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.hgshkt.pokedex"
         minSdk = 30
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
