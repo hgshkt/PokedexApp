5a5c57a761e03243a712bdfe98022faa410e5706
hgshkt <nichtojestvozxc@gmail.com>
2024-08-28 23:43:38 +0300

    detail screen ui is updated

--- app/src/main/java/com/hgshkt/pokedex/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/screens/detail/DetailScreen.kt
@@ -1,21 +1,23 @@
 package com.hgshkt.pokedex.ui.screens.detail
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,6 +28,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -33,6 +38,7 @@ import com.hgshkt.pokedex.ui.custom.ErrorBox
 import com.hgshkt.pokedex.ui.custom.LoadingBox
 import com.hgshkt.pokedex.ui.custom.image.PokemonImage
 import com.hgshkt.pokedex.ui.custom.text.AutoResizedText
+import com.hgshkt.pokedex.ui.data.model.UiPokemon
 import com.hgshkt.pokedex.ui.data.model.UiPokemonAbility
 import com.hgshkt.pokedex.ui.data.model.UiStats
 import com.hgshkt.pokedex.ui.data.model.UiType
@@ -42,6 +48,29 @@ private val pokemonNameStyle = TextStyle(fontSize = 40.sp, fontWeight = FontWeig
 
 private val abilityBackground = Color(0xFFD3F0FF)
 private val statsBorder = Color(0xFFD3F0FF)
+
+@Preview
+@Composable
+fun DetailScreenPreview() {
+    val pokemon = UiPokemon(
+        123, "Pokemon Name",
+        "https://i.pinimg.com/550x/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.jpg",
+        types = listOf(UiType.NORMAL, UiType.ICE),
+        stats = UiStats(
+            12, 54, 23, 61, 14, 63
+        ),
+        height = 100,
+        weight = 99,
+        abilities = listOf(
+            UiPokemonAbility(12, "MyAbility", "Effect details"),
+            UiPokemonAbility(14, "MyAb ility2", "Effect details qwredfa sfafadsfwqfda fadsfqwq"),
+            UiPokemonAbility(15, "MyAbi lity123", "Effect details qwerfdq qi bq ipqubfh qqdfuq ")
+        )
+    )
+    with(pokemon) {
+
+    }
+}
 
 @Composable
 fun DetailScreen(
@@ -71,15 +100,11 @@ fun DetailScreen(
                     modifier = Modifier
                         .verticalScroll(rememberScrollState())
                         .fillMaxWidth()
-                        .padding(24.dp)
+                        .padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = "№$id", style = idStyle)
-                        AutoResizedText(text = name, style = pokemonNameStyle)
-                    }
+                    Text(text = "№$id", style = idStyle)
+                    AutoResizedText(text = name, style = pokemonNameStyle)
                     Spacer(modifier = Modifier.height(8.dp))
                     Row(
                         modifier = Modifier.fillMaxWidth(),
@@ -95,8 +120,12 @@ fun DetailScreen(
                             .fillMaxWidth(),
                         abilities = abilities
                     )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    StatsLayer(stats)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    StatsLayer(
+                        stats = stats,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
                     Spacer(modifier = Modifier.height(12.dp))
                     Row(
                         modifier = Modifier.fillMaxWidth(),
@@ -117,88 +146,113 @@ fun DetailScreen(
 }
 
 @Composable
-private fun PokemonType(type: UiType) {
-    Box(
-        modifier = Modifier
-            .clip(RoundedCornerShape(8.dp))
-            .background(type.backgroundColor)
-            .padding(horizontal = 22.dp, vertical = 12.dp)
-    ) {
-        Text(
-            text = type.text,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold,
-            color = type.textColor
+private fun PokemonType(type: UiType, modifier: Modifier = Modifier) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
         )
-    }
-}
-
-@Composable
-fun Border(content: @Composable () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(16.dp))
-            .border(4.dp, statsBorder, shape = RoundedCornerShape(16.dp))
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        content()
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(type.backgroundColor)
+                .padding(horizontal = 22.dp, vertical = 12.dp)
+        ) {
+            Text(
+                text = type.text,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = type.textColor
+            )
+        }
     }
 }
 
 @Composable
-fun StatsLayer(stats: UiStats) {
-    Border {
-        val statSize = 26.sp
-        with(stats) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-
-                Column {
-                    Text("HP: $hp", fontSize = statSize)
-                    Text("Attack: $attack", fontSize = statSize)
-                    Text("Defense: $defense", fontSize = statSize)
-                }
-                Spacer(modifier = Modifier.width(20.dp))
-                Column {
-                    Text("Sp. Atk: $specialAttack", fontSize = statSize)
-                    Text("Sp. Def: $specialDefense", fontSize = statSize)
-                    Text("Speed: $speed", fontSize = statSize)
-                }
+fun StatsLayer(
+    stats: UiStats,
+    modifier: Modifier = Modifier,
+    textFontSize: TextUnit = 26.sp,
+    contentPadding: PaddingValues = PaddingValues(vertical = 12.dp, horizontal = 20.dp),
+    nameFontWeight: FontWeight = FontWeight.Bold,
+) {
+    with(stats) {
+        ElevatedCard(
+            modifier,
+            colors  = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            )
+        ) {
+            Column(modifier = Modifier.padding(contentPadding)) {
+                Text("HP: $hp", fontSize = textFontSize, fontWeight = nameFontWeight)
+                Text("ATK: $attack", fontSize = textFontSize, fontWeight = nameFontWeight)
+                Text("DEF: $defense", fontSize = textFontSize, fontWeight = nameFontWeight)
+                Text("SPA: $specialAttack", fontSize = textFontSize, fontWeight = nameFontWeight)
+                Text("SPD: $specialDefense", fontSize = textFontSize, fontWeight = nameFontWeight)
+                Text("SPD: $speed", fontSize = textFontSize, fontWeight = nameFontWeight)
+                Spacer(modifier = Modifier.height(12.dp))
+                Text("Total: $total", fontSize = textFontSize, fontWeight = nameFontWeight)
             }
-            Spacer(modifier = Modifier.height(12.dp))
-            Text("Total: $total", fontSize = statSize)
         }
     }
 }
 
 @Composable
 fun AbilityList(modifier: Modifier, abilities: List<UiPokemonAbility>) {
-    Column(
+    ElevatedCard(
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
     ) {
-        Text(text = "Abilities:", fontSize = 26.sp)
-        if (abilities.isNotEmpty()) {
-            abilities.forEach { ability ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 4.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                        .border(3.dp, abilityBackground, RoundedCornerShape(5))
-                        .padding(8.dp),
-                ) {
-                    Text(ability.name, fontSize = 26.sp, fontWeight = FontWeight.Bold)
-                    Text(ability.effect, fontSize = 22.sp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Abilities:", fontSize = 26.sp)
+            if (abilities.isNotEmpty()) {
+                abilities.forEach { ability ->
+                    Ability(
+                        ability = ability,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
                 }
+            } else {
+                Text("Abilities are missing.", fontSize = 20.sp)
             }
-        } else {
-            Text("Abilities are missing.", fontSize = 20.sp)
+        }
+    }
+}
+
+@Composable
+fun Ability(
+    modifier: Modifier = Modifier,
+    ability: UiPokemonAbility,
+    nameFontSize: TextUnit = 26.sp,
+    nameFontWeight: FontWeight = FontWeight.Bold,
+    textFontSize: TextUnit = 22.sp,
+    contentPadding: Dp = 8.dp
+) {
+    ElevatedCard(
+        modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+    ) {
+        Column(Modifier.padding(contentPadding)) {
+            Text(ability.name, fontSize = nameFontSize, fontWeight = nameFontWeight)
+            Text(ability.effect, fontSize = textFontSize)
         }
     }
 }
