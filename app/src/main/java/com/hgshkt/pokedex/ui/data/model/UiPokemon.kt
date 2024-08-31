7a6add9697feb695d146cb67af0d5f656625387d
hgshkt <nichtojestvozxc@gmail.com>
2024-09-01 00:01:10 +0300

    change type color order

--- app/src/main/java/com/hgshkt/pokedex/ui/data/model/UiPokemon.kt
+++ app/src/main/java/com/hgshkt/pokedex/ui/data/model/UiPokemon.kt
@@ -28,24 +28,24 @@ data class UiStats(
 ) : Parcelable
 
 enum class UiType(val text: String, val backgroundColor: Color, val textColor: Color) {
-    NORMAL("NORMAL", Color(0xFFAEA392), Color(0xFFFFFFFF)),
     GROUND("GROUND", Color(0xFF836E64), Color(0xFFFFFFFF)),
+    NORMAL("NORMAL", Color(0xFFAEA392), Color(0xFFFFFFFF)),
     ROCK("ROCK", Color(0xFF767676), Color(0xFFFFFFFF)),
     STEEL("STEEL", Color(0xFF9F9F9F), Color(0xFFFFFFFF)),
-    FLYING("FLYING", Color(0xFF71C3CB), Color(0xFFFFFFFF)),
-    WATER("WATER", Color(0xFF70A1D1), Color(0xFFFFFFFF)),
     ICE("ICE", Color(0xFFA8D3EB), Color(0xFFFFFFFF)),
-    FIRE("FIRE", Color(0xFFEF9566), Color(0xFFFFFFFF)),
-    FIGHTING("FIGHTING", Color(0xFFD17072), Color(0xFFFFFFFF)),
-    DRAGON("DRAGON", Color(0xFFF3726C), Color(0xFFFFFFFF)),
-    ELECTRIC("ELECTRIC", Color(0xFFFAE461), Color(0xFFFFFFFF)),
+    WATER("WATER", Color(0xFF70A1D1), Color(0xFFFFFFFF)),
     GRASS("GRASS", Color(0xFF72D3B4), Color(0xFFFFFFFF)),
+    FLYING("FLYING", Color(0xFF71C3CB), Color(0xFFFFFFFF)),
+    DARK("DARK", Color(0xFF7470D1), Color(0xFFFFFFFF)),
     BUG("BUG", Color(0xFFC8DFA7), Color(0xFFFFFFFF)),
+    ELECTRIC("ELECTRIC", Color(0xFFFAE461), Color(0xFFFFFFFF)),
     POISON("POISON", Color(0xFFA270D1), Color(0xFFFFFFFF)),
+    DRAGON("DRAGON", Color(0xFFF3726C), Color(0xFFFFFFFF)),
+    FIRE("FIRE", Color(0xFFEF9566), Color(0xFFFFFFFF)),
     PHYSIC("PHYSIC", Color(0xFFD170C5), Color(0xFFFFFFFF)),
+    FIGHTING("FIGHTING", Color(0xFFD17072), Color(0xFFFFFFFF)),
     GHOST("GHOST", Color(0xFFBCA5D4), Color(0xFFFFFFFF)),
     FAIRY("FAIRY", Color(0xFFF4C9DA), Color(0xFFFFFFFF)),
-    DARK("DARK", Color(0xFF7470D1), Color(0xFFFFFFFF)),
 }
 
 @Parcelize
