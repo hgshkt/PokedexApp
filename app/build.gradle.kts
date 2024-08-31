382fddd838eb1efb607c6f32b80a3042b3c92ce5
hgshkt <nichtojestvozxc@gmail.com>
2024-09-01 00:01:44 +0300

    version code change to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.hgshkt.pokedex"
         minSdk = 30
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
